05272acbc644c9243cbb3b72a7db29ec8c8888f0
officialrohit928-lang <officialrohit928@gmail.com>
2026-02-08 22:06:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,8 +47,16 @@ STRING5 = os.getenv("STRING_SESSION5", None)
 AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 
 START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
-PING_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+PING_IMG_URL = os.getenv(
+    "PING_IMG_URL",
+    "https://files.catbox.moe/0iapsn.jpg"
+)
+
+PLAYLIST_IMG_URL = os.getenv(
+    "PLAYLIST_IMG_URL",
+    "https://files.catbox.moe/eehxb4.jpg"
+)
+
 STATS_IMG_URL = os.getenv(
     "STATS_IMG_URL",
     "https://files.catbox.moe/eehxb4.jpg"
